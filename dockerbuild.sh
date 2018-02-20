72c0b46dd2e6590c5073679c8a3d2bd8ca87fe33
Daniel Yinanc <danielyinanc@users.noreply.github.com>
2018-02-20 16:11:56 -0500

    sh typo fixed

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-if [ $TRAVIS_BRANCH == 'develop' ]; then
-    docker login -u="devopsicon+autobuilder" -p="$DOCKER_SECRET" quay.io
-    docker build . quay.io/devopsicon/salesmicroservice
-    docker push quay.io/devopsicon/salesmicroservice
-fi
+docker login -u="devopsicon+autobuilder" -p="$DOCKER_SECRET" quay.io
+docker build . quay.io/devopsicon/salesmicroservice
+docker push quay.io/devopsicon/salesmicroservice
