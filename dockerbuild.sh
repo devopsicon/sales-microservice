84f2bc5222e019ae1a35494d235a3c0458b3c361
Daniel Yinanc <danielyinanc@users.noreply.github.com>
2018-02-20 16:06:02 -0500

    Travis typo and Docker Secret URL

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $TRAVIS_BRANCH == 'develop' ]; then
-    docker login -u="devopsicon+autobuilder" -p="$docker-secret" quay.io
+    docker login -u="devopsicon+autobuilder" -p="$DOCKER_SECRET" quay.io
     docker build . quay.io/devopsicon/salesmicroservice
     docker push quay.io/devopsicon/salesmicroservice
 fi
