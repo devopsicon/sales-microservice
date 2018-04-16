72e400858a4c4e9e9f623dc3a772d953eb572dc7
Daniel Yinanc <noemails@noemails.com>
2018-04-16 12:15:55 -0400

    MySQL, FlyWay and Postman
    Changed datasource to mySQL
    Added flyWay migration
    Added POSTMAN collection on how to use

---
+++ src/main/java/com/devopsicon/microservices/sales/configuration/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package com.devopsicon.microservices.sales.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource devDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getRequiredProperty("jdbc.driverClassName"));
+        dataSource.setUrl(env.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(env.getRequiredProperty("jdbc.username"));
+        dataSource.setPassword(env.getRequiredProperty("jdbc.password"));
+        return dataSource;
+
+    }
+}
--- src/main/java/com/devopsicon/microservices/sales/entity/Sale.java
+++ src/main/java/com/devopsicon/microservices/sales/entity/Sale.java
@@ -1,25 +1,27 @@
 package com.devopsicon.microservices.sales.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class Sale {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
     private Float amount;
+    @NotNull
     private Long itemId;
+    @NotNull
     private Long userId;
+    @NotNull
     private Long locationId;
+    @NotNull
     private String comments;
-
-    public Sale(){
-
-    }
 }
